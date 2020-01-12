8e5cb8b8b0d12c6c3ce87e17978aa7f4f181c5f8
Jeddy's Desktop <42729752+tanjd@users.noreply.github.com>
2020-01-13 00:48:10 +0800

    Created forms.py to build form classes
    Python Flask Tutorial: Web Forms with Flask-wtf

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+# Generated secret key for python interpreter
+# import secrets
+# secrets.token_hex(16)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
@@ -14,8 +20,10 @@ posts = [
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
-    },
+    }
 ]
+
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -24,8 +32,29 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title = 'About')
+    return render_template('about.html', title='About')
 
 
-if __name__ == "__main__":
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
